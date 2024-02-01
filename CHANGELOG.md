0b58f6a5a8f1ea8fb7bae233af7673ef453eeb04
jbond <github@johnbond.org>
2024-02-01 16:26:01 +0100

    0.3.0: install ifupdown

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,5 @@
+## 2024-02-01 0.3.0
+* install ifupdown required by network scripts
+
 ## Release 0.1.0 2018 Jul 19
 * initial port from icann-puppet repo
--- manifests/linux.pp
+++ manifests/linux.pp
@@ -10,7 +10,7 @@ class network::linux {
   $prefer_ipv4 = $network::prefer_ipv4
   assert_private()
   create_resources(sysctl, $sysctl)
-  ensure_packages(['vlan'])
+  ensure_packages(['vlan', 'ifupdown'])
 
   if $facts['os']['distro']['codename'] == 'bionic' {
     package { ['ifupdown', 'resolvconf']:
@@ -57,6 +57,7 @@ class network::linux {
     command     => '/sbin/ifup -a',
     subscribe   => File['/etc/network/interfaces'],
     refreshonly => true,
+    require     => Package['ifupdown'],
   }
 
   service { 'networking':
@@ -72,6 +73,7 @@ class network::linux {
       File['/usr/local/bin/network_status.sh'],
       File['/etc/init.d/networking'],
       File['/etc/network/interfaces'],
+      Package['ifupdown'],
     ],
   }
 }
